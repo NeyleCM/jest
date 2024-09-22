78b0cd4e1c4a511cdf26ddf3e9185389d2014bb8
NeyleCM <neylecmorantes@gmail.com>
2024-09-22 19:04:09 +0200

    funciones

--- product.js
+++ product.js
@@ -1,7 +1,7 @@
-let products = [];
-let id = 0;
+let products = [];// Array vacío que contiene los productos
+let id = 0;// Variable que genera un id único para cada producto
 
-// Reinicia la lista de productos
+// Reinicia la lista de productos y el contador de IDs
 function resetProducts() {
     products = [];
     id = 0;
@@ -16,6 +16,35 @@ function addProduct(name, price) {
     products.push({ id, name, price });
 }
 
+// Eliminar un producto por su id
+function removeProduct(productId) {// El ID del producto que se desea eliminar
+    const index = products.findIndex(product => product.id === productId);// Se busca el índice del producto cuyo id coincida con productId.
+    if (index === -1) {//Si el producto no existe (el índice es -1), lanza un mensaje de error
+        throw new Error('Product not found');
+    }
+    products.splice(index, 1);//Si el producto existe, se usa splice para eliminar el producto del array products en el índice encontrado.
+}
+
+// Retorna array de products
+function getProducts() {
+    return products;
+}
+
+// Obtener un producto por su id
+function getProduct(productId) {// Id del producto que se desea obtener.
+    const product = products.find(product => product.id === productId);// find para buscar el producto cuyo id sea igual a productId
+    if (!product) {// Si no encuentra el producto, lanza un mensaje de error
+        throw new Error('Producto no encontrado');
+    }
+    return product;// Si lo encuentra, devuelve el producto.
+}
+
+// Actualizar un producto por su id
+function updateProduct(productId, name, price) {
+    const product = getProduct(productId); //Verifica que el producto existe. Si no existe, lanza un error
+    if (name) product.name = name;// Si el parámetro name es válido (no es nulo), actualiza el nombre del producto
+    if (price) product.price = price;//Si el parámetro price es válido, actualiza el precio del producto
+}
 
 module.exports = { 
     resetProducts,  
